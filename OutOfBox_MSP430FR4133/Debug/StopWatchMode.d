2b81185ca0990218109fcb43d62f2ef431b37a25
corman44 <soccerman44@comcast.net>
2016-05-10 19:58:07 -0700

    Adding Git to CCS

---
+++ OutOfBox_MSP430FR4133/Debug/StopWatchMode.d
@@ -0,0 +1,65 @@
+# FIXED
+
+StopWatchMode.obj: ../StopWatchMode.c
+StopWatchMode.obj: ../StopWatchMode.h
+StopWatchMode.obj: C:/ti/ccsv6/ccs_base/msp430/include/msp430fr4133.h
+StopWatchMode.obj: C:/ti/ccsv6/ccs_base/msp430/include/in430.h
+StopWatchMode.obj: C:/ti/ccsv6/tools/compiler/msp430_15.12.2.LTS/include/intrinsics.h
+StopWatchMode.obj: C:/ti/ccsv6/tools/compiler/msp430_15.12.2.LTS/include/intrinsics_legacy_undefs.h
+StopWatchMode.obj: ../hal_LCD.h
+StopWatchMode.obj: ../main.h
+StopWatchMode.obj: C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/driverlib.h
+StopWatchMode.obj: C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/inc/hw_memmap.h
+StopWatchMode.obj: C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/inc/../deprecated/CCS/msp430fr2xx_4xxgeneric.h
+StopWatchMode.obj: C:/ti/ccsv6/tools/compiler/msp430_15.12.2.LTS/include/stdint.h
+StopWatchMode.obj: C:/ti/ccsv6/tools/compiler/msp430_15.12.2.LTS/include/stdbool.h
+StopWatchMode.obj: C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/adc.h
+StopWatchMode.obj: C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/crc.h
+StopWatchMode.obj: C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/cs.h
+StopWatchMode.obj: C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/eusci_a_spi.h
+StopWatchMode.obj: C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/inc/hw_regaccess.h
+StopWatchMode.obj: C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/eusci_a_uart.h
+StopWatchMode.obj: C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/eusci_b_i2c.h
+StopWatchMode.obj: C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/eusci_b_spi.h
+StopWatchMode.obj: C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/framctl.h
+StopWatchMode.obj: C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/gpio.h
+StopWatchMode.obj: C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/lcd_e.h
+StopWatchMode.obj: C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/pmm.h
+StopWatchMode.obj: C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/rtc.h
+StopWatchMode.obj: C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/sfr.h
+StopWatchMode.obj: C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/sysctl.h
+StopWatchMode.obj: C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/timer_a.h
+StopWatchMode.obj: C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/tlv.h
+StopWatchMode.obj: C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/wdt_a.h
+
+../StopWatchMode.c: 
+../StopWatchMode.h: 
+C:/ti/ccsv6/ccs_base/msp430/include/msp430fr4133.h: 
+C:/ti/ccsv6/ccs_base/msp430/include/in430.h: 
+C:/ti/ccsv6/tools/compiler/msp430_15.12.2.LTS/include/intrinsics.h: 
+C:/ti/ccsv6/tools/compiler/msp430_15.12.2.LTS/include/intrinsics_legacy_undefs.h: 
+../hal_LCD.h: 
+../main.h: 
+C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/driverlib.h: 
+C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/inc/hw_memmap.h: 
+C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/inc/../deprecated/CCS/msp430fr2xx_4xxgeneric.h: 
+C:/ti/ccsv6/tools/compiler/msp430_15.12.2.LTS/include/stdint.h: 
+C:/ti/ccsv6/tools/compiler/msp430_15.12.2.LTS/include/stdbool.h: 
+C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/adc.h: 
+C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/crc.h: 
+C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/cs.h: 
+C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/eusci_a_spi.h: 
+C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/inc/hw_regaccess.h: 
+C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/eusci_a_uart.h: 
+C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/eusci_b_i2c.h: 
+C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/eusci_b_spi.h: 
+C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/framctl.h: 
+C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/gpio.h: 
+C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/lcd_e.h: 
+C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/pmm.h: 
+C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/rtc.h: 
+C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/sfr.h: 
+C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/sysctl.h: 
+C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/timer_a.h: 
+C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/tlv.h: 
+C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/wdt_a.h: 
---
+++ OutOfBox_MSP430FR4133/Debug/main.d
@@ -0,0 +1,67 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: ../main.h
+main.obj: C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/driverlib.h
+main.obj: C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/inc/hw_memmap.h
+main.obj: C:/ti/ccsv6/ccs_base/msp430/include/msp430fr4133.h
+main.obj: C:/ti/ccsv6/ccs_base/msp430/include/in430.h
+main.obj: C:/ti/ccsv6/tools/compiler/msp430_15.12.2.LTS/include/intrinsics.h
+main.obj: C:/ti/ccsv6/tools/compiler/msp430_15.12.2.LTS/include/intrinsics_legacy_undefs.h
+main.obj: C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/inc/../deprecated/CCS/msp430fr2xx_4xxgeneric.h
+main.obj: C:/ti/ccsv6/tools/compiler/msp430_15.12.2.LTS/include/stdint.h
+main.obj: C:/ti/ccsv6/tools/compiler/msp430_15.12.2.LTS/include/stdbool.h
+main.obj: C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/adc.h
+main.obj: C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/crc.h
+main.obj: C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/cs.h
+main.obj: C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/eusci_a_spi.h
+main.obj: C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/inc/hw_regaccess.h
+main.obj: C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/eusci_a_uart.h
+main.obj: C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/eusci_b_i2c.h
+main.obj: C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/eusci_b_spi.h
+main.obj: C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/framctl.h
+main.obj: C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/gpio.h
+main.obj: C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/lcd_e.h
+main.obj: C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/pmm.h
+main.obj: C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/rtc.h
+main.obj: C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/sfr.h
+main.obj: C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/sysctl.h
+main.obj: C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/timer_a.h
+main.obj: C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/tlv.h
+main.obj: C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/wdt_a.h
+main.obj: ../hal_LCD.h
+main.obj: ../StopWatchMode.h
+main.obj: ../TempSensorMode.h
+
+../main.c: 
+../main.h: 
+C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/driverlib.h: 
+C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/inc/hw_memmap.h: 
+C:/ti/ccsv6/ccs_base/msp430/include/msp430fr4133.h: 
+C:/ti/ccsv6/ccs_base/msp430/include/in430.h: 
+C:/ti/ccsv6/tools/compiler/msp430_15.12.2.LTS/include/intrinsics.h: 
+C:/ti/ccsv6/tools/compiler/msp430_15.12.2.LTS/include/intrinsics_legacy_undefs.h: 
+C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/inc/../deprecated/CCS/msp430fr2xx_4xxgeneric.h: 
+C:/ti/ccsv6/tools/compiler/msp430_15.12.2.LTS/include/stdint.h: 
+C:/ti/ccsv6/tools/compiler/msp430_15.12.2.LTS/include/stdbool.h: 
+C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/adc.h: 
+C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/crc.h: 
+C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/cs.h: 
+C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/eusci_a_spi.h: 
+C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/inc/hw_regaccess.h: 
+C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/eusci_a_uart.h: 
+C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/eusci_b_i2c.h: 
+C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/eusci_b_spi.h: 
+C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/framctl.h: 
+C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/gpio.h: 
+C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/lcd_e.h: 
+C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/pmm.h: 
+C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/rtc.h: 
+C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/sfr.h: 
+C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/sysctl.h: 
+C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/timer_a.h: 
+C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/tlv.h: 
+C:/Users/coryj/Documents/GitHub/CCS/OutOfBox_MSP430FR4133/driverlib/MSP430FR2xx_4xx/wdt_a.h: 
+../hal_LCD.h: 
+../StopWatchMode.h: 
+../TempSensorMode.h: 
